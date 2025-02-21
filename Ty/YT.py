51369ff61340c7a2a3385b862ba576f922db92ce
AdityaSharma2403 <167861948+AdityaSharma2403@users.noreply.github.com>
2025-02-21 13:19:57 +0530

    Create file YT.py

---
+++ Ty/YT.py
@@ -0,0 +1,7 @@
+import tensorflow as tf  # TensorFlow  
+import torch  # PyTorch  
+from transformers import pipeline  # Hugging Face Transformers  
+import spacy  # spaCy  
+import cv2  # OpenCV  
+
+print("Done")
